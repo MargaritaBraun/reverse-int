5442719064b65eb14aa28e02a096c9e082bd08a4
Marharyta Parkalava <margobraun777@gmail.com>
2023-08-21 03:24:30 +0300

    fix: welldone reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const check = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let arr = Array.from(String(n), Number);
+    let newarr = arr.map((item => {
+        if(check.includes(item)){
+            return +item
+        } else {
+            item
+        }
+    }))
+    return newarr.reverse().join('')
 }
